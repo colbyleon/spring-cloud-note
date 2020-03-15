ba423ad5ffc1766a71affb259fce2edf5adf6eef
colbyleon <616349924@qq.com>
2020-03-15 17:42:56 +0800

    sleuth

---
+++ gateway/src/main/java/com/dsky/scdemo/gateway/filter/sleuth/TraceIdFilter.java
@@ -0,0 +1,39 @@
+package com.dsky.scdemo.gateway.filter.sleuth;
+
+import brave.Span;
+import brave.Tracer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
+
+/**
+ * @author colby
+ * @date 2020/3/15 11:46
+ */
+@Component
+public class TraceIdFilter implements WebFilter {
+
+    @Resource
+    private Tracer tracer;
+
+    @Override
+    @NonNull
+    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
+        exchange.getResponse().beforeCommit(() -> {
+            Span currentSpan = tracer.currentSpan();
+            if (currentSpan != null) {
+                String traceId = currentSpan.context().traceIdString();
+                exchange.getResponse().getHeaders().add("TRACE-ID", traceId);
+            }
+            return Mono.empty();
+        });
+        return chain.filter(exchange);
+    }
+}
